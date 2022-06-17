39a41ea54a2296ccb2852106dd66496b80e7e484
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-16 21:17:24 -0400

    Experimenting with React.memo()

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import Button from "./components/UI/Button/Button";
+import DemoOutput from "./components/Demo/DemoOutput";
 
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
 
-  console.log("app running");
-
   const toggleParagraphHandler = (e) => {
     setShowParagraph((showParagraph) => !showParagraph);
   };
 
+  console.log("App runs");
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
+      <DemoOutput show={false} />
       <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
